74a6dedb3ffe2287ea5b91b4801220eafe5a5394
TZU-CHAO WANG <john29917958@gmail.com>
2024-03-16 16:43:56 +0800

    Add mongoose unique validator for User entity to provide validation errors when unique constraint violated

--- JavaScript/Beginning Node.js, Express & MongoDB Development/blog-project/models/User.js
+++ JavaScript/Beginning Node.js, Express & MongoDB Development/blog-project/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const uniqueValidator = require("mongoose-unique-validator");
 const bcrypt = require("bcrypt");
 
 const UserSchema = new Schema({
@@ -14,6 +15,7 @@ const UserSchema = new Schema({
   },
 });
 
+UserSchema.plugin(uniqueValidator);
 UserSchema.pre("save", function (next) {
   const user = this;
 
