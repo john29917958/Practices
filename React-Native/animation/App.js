997d107c702fb05c5507407206bdd5cc5debe6b8
TZU-CHAO WANG <john29917958@gmail.com>
2024-04-14 15:41:26 +0800

    Add animation React Native Expo project

---
+++ React-Native/animation/App.js
@@ -0,0 +1,47 @@
+import React, { useRef, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Animated, Easing, Text, View } from "react-native";
+
+const FadeInView = (props) => {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      delay: 1000,
+      toValue: 1,
+      easing: Easing.back(),
+      duration: 3000,
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim]);
+
+  return (
+    <Animated.View style={{ ...props.style, opacity: fadeAnim }}>
+      {props.children}
+    </Animated.View>
+  );
+};
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <FadeInView
+        style={{ width: 250, height: 50, backgroundColor: "powderblue" }}
+      >
+        <Text style={{ fontSize: 28, textAlign: "center", margin: 10 }}>
+          Fading in
+        </Text>
+      </FadeInView>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
