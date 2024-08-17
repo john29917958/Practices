a86ed04c8aaf7894bb9fde3bae623d3d596b55d8
TZU-CHAO WANG <john29917958@gmail.com>
2024-08-17 10:01:41 +0800

    Format the output of array elements

--- C++/bubble-sort.cpp
+++ C++/bubble-sort.cpp
@@ -29,29 +29,28 @@ void sort(int arr[], int size)
 
 void print_array(int arr[], int size)
 {
-    std::string message = "Array: [";
+    std::cout << "[";
     if (size > 0)
     {
-        message += std::to_string(arr[0]);
+        std::cout << arr[0];
         for (int i = 1; i < size; i++)
         {
-            message += ", " + std::to_string(arr[i]);
+            std::cout << ", " << arr[i];
         }
     }
-    message += "]";
-    std::cout << message;
+    std::cout << "]";
 }
 
-int main(int argc, char argv[])
+int main(int argc, char** argv)
 {
     std::cout << "Welcome to a bubble sort program." << std::endl;
     int arr[] = {6, 1, 5, 2, 4, 3};
     int arr_size = sizeof(arr) / sizeof(arr[0]);
-    std::cout << "Prints array before sort." << std::endl;
+    std::cout << "Array before sort: ";
     print_array(arr, arr_size);
     std::cout << std::endl;
     sort(arr, arr_size);
-    std::cout << "Prints array after sort." << std::endl;
+    std::cout << "Array after sort: ";
     print_array(arr, arr_size);
 
     return 0;
