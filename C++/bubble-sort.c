7907fb0d752ee29720544b1769c7cb08fac7e76d
TZU-CHAO WANG <john29917958@gmail.com>
2024-08-17 09:57:59 +0800

    Rewrite C version of bubble sort program

--- C++/bubble-sort.c
+++ C++/bubble-sort.c
@@ -1,38 +1,56 @@
 #include "stdio.h"
 
-void sortArray(int *arrayToSort, int arrayLength)
+void swap(int index_1, int index_2, int arr[])
 {
-    int isAnySortingThisRound = 0;
+    int num_cache = arr[index_1];
+    arr[index_1] = arr[index_2];
+    arr[index_2] = num_cache;
+}
+
+void sort(int arr[], int size)
+{
+    int is_sorted_this_round = 0;
 
     do
     {
-        isAnySortingThisRound = 0;
-        for (int i = 0; i < arrayLength - 1; i++)
-        {
-            int currentIndex = i;
-            int nextIndex = i + 1;
+        is_sorted_this_round = 0;
 
-            if (arrayToSort[currentIndex] > arrayToSort[nextIndex])
+        for (int i = 0; i < size - 1; i++)
+        {
+            if (arr[i] > arr[i + 1])
             {
-                isAnySortingThisRound = 1;
-                int tempNextElemValOfCurr = arrayToSort[nextIndex];
-                arrayToSort[nextIndex] = arrayToSort[currentIndex];
-                arrayToSort[currentIndex] = tempNextElemValOfCurr;
+                swap(i, i + 1, arr);
+                is_sorted_this_round = 1;
             }
         }
-    } while (isAnySortingThisRound == 1);
+    } while (is_sorted_this_round == 1);
 }
 
-int main(int argc, char argv[])
+void print_arr(int arr[], int size)
 {
-    int array[] = {2, 7, 3, 8};
-    int arraySize = sizeof(array) / sizeof(array[0]);
-    sortArray(array, arraySize);
-    for (int i = 0; i < arraySize; i++)
+    printf("[");
+    if (size > 0)
     {
-        printf("%d", array[i]);
+        printf("%d", arr[0]);
+        for (int i = 1; i < size; i++)
+        {
+            printf(", %d", arr[i]);
+        }
     }
+    printf("]");
+}
+
+int main(int argc, char **argv)
+{
+    printf("Bubble sort program starts!\n");
+    int arr[] = {6, 1, 5, 2, 4, 3};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    printf("Array before sort: ");
+    print_arr(arr, size);
     printf("\n");
+    sort(arr, size);
+    printf("Array after sort: ");
+    print_arr(arr, size);
 
     return 0;
 }
